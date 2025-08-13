71c72863a4f9b4fae1478fdf569a9a89be574f1e
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2025-08-13 18:54:05 +0600

    Refactor client-server application to enable real-time message exchange using threading for concurrent message handling

--- 3. Write a client-server application using TCP sockets that allows users to exchange messages in real-time/client.py
+++ 3. Write a client-server application using TCP sockets that allows users to exchange messages in real-time/client.py
@@ -1,11 +1,28 @@
 import socket
+import threading
 
 HOST = 'localhost'
 PORT = 12345
 
+def receive_messages(s):
+    while True:
+        try:
+            message = s.recv(1024).decode()
+            if not message:
+                break
+            print(f"Server: {message}")
+        except:
+            break
+
+def send_messages(s):
+    while True:
+        try:
+            msg = input("")
+            s.sendall(msg.encode())
+        except:
+            break
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    message = input("Enter a message: ")
-    s.sendall(message.encode())
-    data = s.recv(1024)
-    print("Echo from server:", data.decode())
+    threading.Thread(target=receive_messages, args=(s,)).start()
+    threading.Thread(target=send_messages, args=(s,)).start()
